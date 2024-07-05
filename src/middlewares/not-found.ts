17db500a29b05a0bb992eb9755b539a2eff43dac
manasdeo <rmnslogin@gmail.com>
2024-07-05 22:09:43 +0530

    Reseting the code

--- src/middlewares/not-found.ts
+++
@@ -1,6 +0,0 @@
-import { type Request, type Response } from "express";
-
-const notFound = (req: Request, res: Response) =>
-  res.status(404).send("Route does not exist");
-
-export default notFound;
