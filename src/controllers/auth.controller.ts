a645d2e08f593170febe0fbdd795504cc075b6e5
manasdeo <rmnslogin@gmail.com>
2024-07-11 10:22:01 +0530

    admin logic and password hashing

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,13 +1,21 @@
 import { type Request, type Response } from "express";
 import User from "../models/user.model";
 import { StatusCodes } from "http-status-codes";
+import CustomError from "../errors";
 
 const register = async (req: Request, res: Response) => {
   const { email, name, password } = req.body;
-  let user;
-  if (email && !(await User.findOne({ email })) && name && password)
-    user = await User.findOne({ email });
-  res.send(user);
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists)
+    throw new CustomError.BadRequestError("Email already exists");
+
+  // First registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ email, name, password, role });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req: Request, res: Response) => {
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,11 +1,13 @@
-import mongoose, { Model, Schema } from "mongoose";
+import mongoose, { Model, Schema, Document } from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
 
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
   role: "admin" | "user";
+  comparePassword: (canidatePassword: string) => Promise<boolean>;
 }
 
 // const UserSchema: Schema<IUser> = new Schema({ ... });: This creates a new Schema object called UserSchema and defines the structure of the User document. The Schema type is parameterized with the IUser interface, ensuring that the schema matches the defined interface.
@@ -19,9 +21,10 @@ const UserSchema: Schema<IUser> = new mongoose.Schema<IUser>({
 
   email: {
     type: String,
+    unique: true,
     required: [true, "Please provide email"],
     validate: {
-      validator: function (value: string) {
+      validator: async function (value: string) {
         return validator.isEmail(value);
       },
       message: "Please provide an email",
@@ -40,6 +43,21 @@ const UserSchema: Schema<IUser> = new mongoose.Schema<IUser>({
     default: "user",
   },
 });
+
+UserSchema.pre("save", async function (this: IUser) {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+});
+
+UserSchema.methods.comparePassword = async function (
+  this: IUser,
+  canidatePassword: string
+) {
+  const isMatch = await bcrypt.compare(canidatePassword, this.password);
+  return isMatch;
+};
 
 interface IUserModel extends Model<IUser> {}
 
